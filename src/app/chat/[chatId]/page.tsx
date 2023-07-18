abaa57904c3125eea9bb9ca042ec371f28bbb2dc
kaduh15 <kadu.silva2014@gmail.com>
2023-07-18 18:37:13 -0300

    ✨ feat(page.tsx): add ChatId component to render a chat page with chatId parameter 🔧 chore(page.tsx): import necessary icons for ChatId component

---
+++ src/app/chat/[chatId]/page.tsx
@@ -0,0 +1,24 @@
+import { BsEmojiSmileFill } from 'react-icons/bs'
+import { GoPaperclip } from 'react-icons/go'
+import { IoCamera } from 'react-icons/io5'
+
+type ChatIdProps = {
+  params: { chatId: string }
+}
+
+export default async function ChatId({ params }: ChatIdProps) {
+  return (
+    <div className="flex flex-1 flex-col justify-between bg-stone-300 p-3">
+      <div className="flex-1">chat: {params.chatId}</div>
+      <div className="flex items-center justify-center gap-5 rounded-full bg-white p-4">
+        <BsEmojiSmileFill className="h-5 w-5" />
+        <input
+          type="text"
+          className="flex-1 border-b-2 border-white outline-none transition-all focus:border-neutral-400"
+        />
+        <GoPaperclip className="h-5 w-5" />
+        <IoCamera className="h-5 w-5" />
+      </div>
+    </div>
+  )
+}
